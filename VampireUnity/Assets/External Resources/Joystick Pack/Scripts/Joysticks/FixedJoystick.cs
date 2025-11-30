8433c3e976bd39101b40958151ae34fb69d4896e
wangweikang3 <wangweikang3@tal.com>
2025-11-30 08:43:51 +0800

    5

---
+++ VampireUnity/Assets/External Resources/Joystick Pack/Scripts/Joysticks/FixedJoystick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FixedJoystick : Joystick
+{
+    private Vector2 handleStartPosition;
+
+    protected override void Start()
+    {
+        base.Start();
+        handleStartPosition = handle.anchoredPosition;
+    }
+
+    public new void OnDrag(PointerEventData eventData)
+    {
+        cam = null;
+        if (canvas.renderMode == RenderMode.ScreenSpaceCamera)
+            cam = canvas.worldCamera;
+
+        Vector2 position = RectTransformUtility.WorldToScreenPoint(cam, background.position);
+        Vector2 radius = background.sizeDelta / 2;
+        input = (eventData.position - position) / (radius * canvas.scaleFactor);
+        FormatInput();
+        HandleInput(input.magnitude, input.normalized, radius, cam);
+        handle.anchoredPosition = handleStartPosition + input * radius * handleRange;
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        input = Vector2.zero;
+        handle.anchoredPosition = handleStartPosition;
+    }
+}
